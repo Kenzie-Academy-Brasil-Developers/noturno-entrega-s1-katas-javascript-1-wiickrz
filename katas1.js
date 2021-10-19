3d34bca5e495c311681d0f17844a8cbdd521719d
wiickrz <will.cs.182@gmail.com>
2021-10-19 19:42:05 -0300

    Exercícios feitos

---
+++ katas1.js
@@ -0,0 +1,164 @@
+     //  Entrega - Aula 42 - William Heckler                                     
+
+        /*
+            Exercise 1
+        */
+
+// let oneThroughTwenty = () => {
+//     let result = []
+//     for (let counter = 1; counter <= 20; counter++) {
+//     result.push(counter)
+//     }
+//     return result
+// }
+// console.log(oneThroughTwenty())
+
+
+        /*
+            Exercise 2
+        */
+
+
+// let evensToTwenty = () => {
+//     let result = []
+//     for (let counter = 1; counter <= 20; counter++) {
+//         if (counter % 2 === 0) {
+//             result.push(counter)
+//         }
+//     }
+//     return result
+// }
+// console.log(evensToTwenty())
+
+
+        /*
+            Exercise 3
+        */
+
+
+// let oddsToTwenty = () => {
+//     let result = []
+//     for (let counter = 1; counter <= 20; counter++) {
+//         if (counter % 2 !== 0) {
+//             result.push(counter)
+//         }
+//     }
+//     return result
+// }
+// console.log(oddsToTwenty())
+
+
+        /*
+            Exercise 4
+        */
+
+
+// let multiplesOfFive = () => {
+//     let result = []
+//     let counter = 1
+//     while (counter <= 100) {
+//         if (counter % 5 === 0) {
+//             result.push(counter)
+//         }
+//         counter++
+//     }
+//     return result
+// }
+// console.log(multiplesOfFive())
+
+
+        /*
+            Exercise 5
+        */
+
+// let squareNumbers = () => {
+//     let result = []
+//     for (let counter = 1; counter <= 10; counter++) {
+//         result.push(counter * counter)
+//     }
+//     return result
+// }
+// console.log(squareNumbers())
+
+
+        /*
+            Exercise 6
+        */
+
+
+// let countingBackwards = () => {
+//     let result = []
+//     for (let counter = 20; counter >= 1; counter--) {
+//     result.push(counter)
+//     }
+//     return result
+// }
+// console.log(countingBackwards())
+
+
+        /*
+            Exercise 7
+        */
+
+
+// let evenNumbersBackwards = () => {
+//     let result = []
+//     for (let counter = 20; counter >= 1; counter--) {
+//         if (counter % 2 === 0) {
+//             result.push(counter)
+//         }
+//     }
+//     return result
+// }
+// console.log(evenNumbersBackwards())
+
+
+        /*
+            Exercise 8
+        */
+
+
+// let oddNumbersBackwards = () => {
+//     let result = []
+//     for (let counter = 20; counter >= 1; counter--) {
+//         if (counter % 2 !== 0) {
+//             result.push(counter)
+//         }
+//     }
+//     return result
+// }
+// console.log(oddNumbersBackwards())            
+
+
+        /*
+            Exercise 9
+        */
+
+
+// let multiplesOfFiveBackwards = () => {
+//     let result = []
+//     for (let counter = 100; counter >= 1; counter--) {
+//         if (counter % 5 === 0) {
+//             result.push(counter)
+//         }
+//     }
+//     return result
+// }
+// console.log(multiplesOfFiveBackwards())
+
+
+        /*
+            Exercise 10
+        */
+
+        
+// let squareNumbersBackwards = () => {
+//     let result = []
+//     let counter = 10
+//     while (counter >= 1) {
+//         result.push(counter * counter)
+//         counter--
+//     }
+//     return result
+// }
+// console.log(squareNumbersBackwards())
